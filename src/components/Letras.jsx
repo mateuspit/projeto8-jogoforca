14ec5709f20bbc11d173837e4b94401df5dd64b3
Mateus Barcelos <mateuspit@hotmail.com>
2023-02-15 16:32:57 -0300

    first styled components use: letters ready for choice in hangman game

--- src/components/Letras.jsx
+++ src/components/Letras.jsx
@@ -1,17 +1,18 @@
-import LettersStyle from "../styles";
+import { LetterStyleBoxOn, AllLettersSpot } from "../styles";
 
 export default function Letras() {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     return (
         <>
-            {alfabeto.map((w, index) => (
-                <div className="word">
-                    <LettersStyle>
+            <AllLettersSpot>
+                {alfabeto.map((w, index) => (
+                    <LetterStyleBoxOn>
                         <Letra key={index} letra={w} />
-                    </LettersStyle>
-                </div>
-            ))}
+                    </LetterStyleBoxOn>
+                ))}
+            </AllLettersSpot>
         </>
+
     );
 }
 
--- src/styles.js
+++ src/styles.js
@@ -1,9 +1,26 @@
 import styled from "styled-components";
 
-const LettersStyle = styled.div`
-background-color: red;
-
+export const LetterStyleBoxOn = styled.div`
+    background-color: #E1ECF4;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #39739D;
+    border-radius: 3px;
+    border: solid 1px #7AA7C7;
+    margin-right: 12px;
 `;
 
+export const AllLettersSpot = styled.div`
+    width: 750px;
+    height: 91px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 152px;
+    margin-top: 70px;
+`;
 
-export default LettersStyle
